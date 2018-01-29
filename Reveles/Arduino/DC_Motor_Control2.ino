55884d10334cc3f1fd1eeac565a9207e11bbb2bc
fehittel14 <34112360+fehittel14@users.noreply.github.com>
2018-01-29 16:53:54 +0000

    Update DC_Motor_Control2.ino
    This is an updated version of the beta motor control program. This program allows to take a ultrasonic reading from the core and decides whether the robot will move forward or backward. Still in the development phase. Turning functions are WIP.

--- Reveles/Arduino/DC_Motor_Control2.ino
+++ Reveles/Arduino/DC_Motor_Control2.ino
@@ -3,13 +3,13 @@
 #define enableA 10
 #define input1 11
 #define input2 12
-#define b1 13
 #define trigPIN1 1
 #define echoPIN1 2
 #define tof1 3
 #define pir1 4
 
 int rD = 0;
+int destination
 bool pressed = false;
 
 void setup() {
@@ -19,54 +19,51 @@ void setup() {
   pinMode(input1, OUTPUT);
   pinMode(input2, OUTPUT);
   pinMode(b1, INPUT);
-  pinMode(trigPIN1, OUTPUT);
-  pinMode(echoPIN1, INPUT);
-  // Used for initial startup
+  
+  // Used for initial motor startup
   digitalWrite(enableA, LOW);
-  digitalWrite(input1, HIGH);
-  digitalWrite(input2, HIGH);
+  digitalWrite(input1, LOW);
+  digitalWrite(input2, LOW);
 }
 
 void loop()
 { 
-  int destination = 0;
-  int duration, distance;
+  int destination == 0
   do
   {
-    digitalWrite(trigPIN1, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(trigPIN1, LOW);
-    duration = pulseIn(echoPIN1, HIGH);
-    distance = (duration/2)/12; 
-    
-    if(enableA == LOW)
-    {
-      motorON();
-      }
-    
-    forward(distance);
+    start();
   }
  }while(destination == 0)
 }
+
+/*This function is designed for use in the initial boot-up of the robot. The motors will be turned on if not already and begin moving forward,
+FUTURE ITERATIONS SHOULD INVOLVE A DECISION TO CHANGE ITS STARTING DIRECTION!*/
+void start()
+{
+  if(enable == LOW)
+  {
+    motorON();
+   }
+  forward();
+  }
 /*This function is designed to enable the robot to move forward. The inputs are adjusted to
 the orientation that spins the motors forward and begins incrementing*/
-void forward(int x)
+void forward()
 {
   digitalWrite(input1, HIGH);
   digitalWrite(input2, LOW);
-  delay(20);
-  incrementsFORWARD(x);
-}
-
+  delayMicroseconds(1000);
+  driveFORWARD();
+  }
 /*This function is designed to enable the robot to move forward. The inputs are adjusted to
 the orientation that spins the motors backward and begins incrementing*/
-void backward(int x)
+void backward()
 {
    digitalWrite(input1, LOW);
    digitalWrite(input2, HIGH);
    delay(20);
-   incrementsBACKWARD(x);
-}
+   driveBACKWARD();
+  }
 /*This function is designed to stop movement while still ready to move. The input pins will be turned high to prevent current flow,
 resulting in the robot's inability to move but can begin once a proper current path is made. */
 void motorSTOP()
@@ -74,274 +71,56 @@ void motorSTOP()
   digitalWrite(input1, HIGH);
   digitalWrite(input2, HIGH);
   delay(20);
-}
+  }
 /*This function is designed for use in the initial startup of the robot. When the robot is booted up, the enable pin on the motor
 drivers will be turned high so that the robot may begin moving.*/
 void motorON()
 {
   digitalWrite(enableA, HIGH);
   delay(20);
-}
-
+  }
 /*This function is designed as the Emergency Stop for the robot. When the time-of-flight sensor is flagged, the robot will shut off.*/
 void motorOFF()
 {
   digitalWrite(enableA, LOW);
-}
-
+  }
 /*This function is designed to accelerate and deccelerate the motors in increments\
 while moving forward. There are two individual incremental settings based on the signals
 received by the PIR sensor and the time-of-flight sensors. If neither sensors are flagged 
 (e.g. only sees a wall), then the function will go through the slower progression. If either
 sensor is flagged, the function will go into the the steeper section. If the time-of-flight sensor
 is flagged, then the system will go into an emergency stop function. */
-void incrementsFORWARD(int x, int y, int z)
+void driveFORWARD()
 {
-  for(int i; z = 1; i++)
-  {
-  if(z == 1)
+  int pwmOUT = (distUS1 * 2)-1;
+  if(pwmOUT < 48)
   {
     motorOFF();
+    backward();
    }
-  else  
-  digitalWrite(trigPIN1, HIGH);
-  delayMicroseconds(1000);
-  digitalWrite(trigPIN1, LOW);
-  int d = pulseIn(echoPIN1, HIGH);
-  x = (d/2)/12; 
-  //y = code to register a 1 or a 0
-  y = 0;
-  if (y == 0)
+  else if (pwmOUT > 255)
   {
-   if (x > 12)
-   {
-    analogWrite(enableA,255);
-    } 
-   else if(11 < x < 12)
-   {
-    analogWrite(enableA, 230);
-    } 
-   else if(10 < x < 11)
-   {
-    analogWrite(enableA, 205);
-    }
-   else if(9 < x < 10)
-   {
-    analogWrite(enableA, 180);
-    }
-   else if(8 < x < 9)
-   {
-    analogWrite(enableA, 155);
-    }
-   else if(7 < x < 8)
-   {
-    analogWrite(enableA,130);
-    } 
-   else if(6 < x < 7)
-   {
-    analogWrite(enableA, 105);
-    } 
-   else if(5 < x < 6)
-   {
-    analogWrite(enableA, 80);
-    }
-   else if(x > 4 && x < 5)
-   {
-    analogWrite(enableA, 55);
-    }
-   else if(x > 3 && x < 4)
-   {
-    analogWrite(enableA, 30);
-    }
-   else
-   {
-    analogWrite(enableA, 0);
-    motorSTOP();
-    backward(x);
-    }
+    pwmOUT = 255;
    }
-  else
-  {
-    if (x > 12)
-   {
-    analogWrite(enableA,255);
-    } 
-   else if(x > 11 && x < 12)
-   {
-    analogWrite(enableA, 223);
-    } 
-   else if(x > 10 && x < 11)
-   {
-    analogWrite(enableA, 191);
-    }
-   else if(x > 9 && x < 10)
-   {
-    analogWrite(enableA, 159);
-    }
-   else if(x > 8 && x < 9)
-   {
-    analogWrite(enableA, 127);
-    }
-   else if(x > 7 && x < 8)
-   {
-    analogWrite(enableA,95);
-    } 
-   else if(x > 6 && x < 7)
-   {
-    analogWrite(enableA, 63);
-    }
-   else if(x > 5 && x < 6)
-   {
-    analogWrite(enableA, 31);
-    }
-   else
-   {
-    analogWrite(enableA, 0);
-    motorSTOP();
-    backward(x);
-    }
-   }
-  }
+  analogWrite(enableA,pwmOUT);
  }
- 
 /*This function is designed to accelerate and deccelerate the motors in increments\
 while moving backward. There are two individual incremental settings based on the signals
 received by the PIR sensor and the time-of-flight sensors. If neither sensors are flagged 
 (e.g. only sees a wall), then the function will go through the slower progression. If either
 sensor is flagged, the function will go into the the steeper section. If the time-of-flight sensor
 is flagged, then the system will go into an emergency stop function. */
-void incrementsBACKWARD(int x, int y, int z)
+void driveBACKWARD()
 {
-  
-  for(int i; z = 1; i++)
+  int pwmOUT = (distUS2 * 2)-1;
+  if(pwmOUT < 48)
   {
-  if(z == 1)
-  {
-    motorOFF();
-   }
-  else  
-  digitalWrite(trigPIN1, HIGH);
-  delayMicroseconds(1000);
-  digitalWrite(trigPIN1, LOW);
-  int d = pulseIn(echoPIN1, HIGH);
-  x = (d/2)/12; 
-  //y = code to register a 1 or a 0
-  y = 0;
-  if (y == 0)
-  {
-   if (x > 12)
-   {
-    analogWrite(enableA,255);
-    } 
-   else if(11 < x < 12)
-   {
-    analogWrite(enableA, 230);
-    } 
-   else if(10 < x < 11)
-   {
-    analogWrite(enableA, 205);
-    }
-   else if(9 < x < 10)
-   {
-    analogWrite(enableA, 180);
-    }
-   else if(8 < x < 9)
-   {
-    analogWrite(enableA, 155);
-    }
-   else if(7 < x < 8)
-   {
-    analogWrite(enableA,130);
-    } 
-   else if(6 < x < 7)
-   {
-    analogWrite(enableA, 105);
-    } 
-   else if(5 < x < 6)
-   {
-    analogWrite(enableA, 80);
-    }
-   else if(x > 4 && x < 5)
-   {
-    analogWrite(enableA, 55);
-    }
-   else if(x > 3 && x < 4)
-   {
-    analogWrite(enableA, 30);
-    }
-   else
-   {
-    analogWrite(enableA, 0);
     motorSTOP();
-    forward(x);
-    }
+    backward();
    }
-  else
+  else if (pwmOUT > 255)
   {
-    if (x > 12)
-   {
-    analogWrite(enableA,255);
-    } 
-   else if(x > 11 && x < 12)
-   {
-    analogWrite(enableA, 223);
-    } 
-   else if(x > 10 && x < 11)
-   {
-    analogWrite(enableA, 191);
-    }
-   else if(x > 9 && x < 10)
-   {
-    analogWrite(enableA, 159);
-    }
-   else if(x > 8 && x < 9)
-   {
-    analogWrite(enableA, 127);
-    }
-   else if(x > 7 && x < 8)
-   {
-    analogWrite(enableA,95);
-    } 
-   else if(x > 6 && x < 7)
-   {
-    analogWrite(enableA, 63);
-    }
-   else if(x > 5 && x < 6)
-   {
-    analogWrite(enableA, 31);
-    }
-   else
-   {
-    analogWrite(enableA, 0);
-    motorSTOP();
-    forward(x);
-    }
+    pwmOUT = 255;
    }
+  analogWrite(enableA,pwmOUT);
   }
- }
-
-
-//  // Read button - Debounce
-//  if (digitalRead(b1) == true) 
-//  {
-//    pressed = !pressed;
-//  }
-//  while (digitalRead(b1) == true);
-//  delay(20);
-//  
-//  //Motor Direction Control with Button
-//  // If button is pressed - change rotation direction
-//  if (pressed == true  & rD == 0) 
-//  {
-//    digitalWrite(input1, HIGH);
-//    digitalWrite(input2, LOW);
-//    rD = 1;
-//    delay(20);
-//  }
-//  // If button is pressed - change rotation direction
-//  if (pressed == false & rD == 1) {
-//    digitalWrite(input1, LOW);
-//    digitalWrite(input2, HIGH);
-//    rD = 0;
-//    delay(20);
-//  }
